b5c06d9f82f4736721ba78649abc181ad680b704
Loïc <loic@thoughtworks.com>
2016-03-20 15:25:00 +0800

    [loic] Update initial render

--- js/app.jsx
+++ js/app.jsx
@@ -1,6 +1,8 @@
 (function () {
 	'use strict';
 
+	var ENTER_KEY = 13;
+
 	class TodoApp extends React.Component {
 		constructor(props) {
 			super(props);
@@ -11,12 +13,16 @@
 				<div>
 					<header className="header">
 						<h1>todos</h1>
-						<input
-							className="new-todo"
-							placeholder="What needs to be done?" />
+						<input className="new-todo" 
+							   placeholder="What needs to be done?" 
+							   autoFocus={true} />
 					</header>
 					<section className="main">
-
+						<input className="toggle-all" 
+							   type="checkbox" />
+						<ul className="todo-list">
+						
+						</ul>
 					</section>
 				</div>
 			);
