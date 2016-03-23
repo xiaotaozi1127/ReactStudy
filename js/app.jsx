d6c20ae9a68033ca718841e13a0632bafcdd0c9d
twer <xiaotaozi1127@hotmail.com>
2016-03-23 09:05:34 +0800

    taohui add todoitem, mark one as done, mark all as done.

--- js/app.jsx
+++ js/app.jsx
@@ -6,26 +6,110 @@
 	class TodoApp extends React.Component {
 		constructor(props) {
 			super(props);
+
+			this.state = {
+				todos: [],
+				newtodo: ""
+			}
+		}
+
+		onNewTodoChange(event){
+			this.setState({
+				newtodo: event.target.value
+			})
+		}
+
+		onTodoKeyDown(event){			
+			if(event.keyCode !== ENTER_KEY){
+				return;
+			}
+			var todolist = this.state.todos;
+			var value = this.state.newtodo.trim();
+
+			if(value !== ""){
+				todolist.push({
+					id: todolist.length + 1,
+					title: this.state.newtodo.trim(),
+					completed: false
+				});
+				this.setState({
+					todos: todolist,
+					newtodo: ""
+				});
+			}			
+		}
+
+		onCheckAllTodosChanged(event){
+			var todolist = this.state.todos;
+			todolist.forEach(todo => {
+				todo.completed = event.target.checked;
+			});
+			this.setState({
+				todos: todolist
+			});			
 		}
 
 		render() {
+			var todoitems = this.state.todos.map(todo =>{
+				return <ToDoItem todo={todo} />
+			});
+
 			return (
 				<div>
 					<header className="header">
 						<h1>todos</h1>
 						<input className="new-todo" 
 							   placeholder="What needs to be done?" 
-							   autoFocus={true} />
+							   autoFocus={true} 
+							   value = {this.state.newtodo}
+							   onChange = {this.onNewTodoChange.bind(this)}
+							   onKeyDown = {this.onTodoKeyDown.bind(this)}
+							   />
 					</header>
 					<section className="main">
 						<input className="toggle-all" 
-							   type="checkbox" />
+							   type="checkbox"
+							   onChange = {this.onCheckAllTodosChanged.bind(this)}
+							   />
 						<ul className="todo-list">
-						
+						   {todoitems}
 						</ul>
 					</section>
 				</div>
 			);
+		}
+	}
+
+	class ToDoItem extends React.Component{
+		constructor(props) {
+			super(props);
+			this.state = {
+				completed: this.props.todo.completed
+			}
+		}
+
+		onTodoItemCheckedChange(event){
+			this.setState({
+				completed: event.target.checked
+			});
+		}
+
+		componentWillReceiveProps(nextProps){			
+			console.log("nextProps.todo.completed " + nextProps.todo.completed);
+			this.setState({
+				completed: nextProps.todo.completed
+			});			
+		}
+
+		render(){
+			return <li className={this.state.completed && "completed"}>
+			    <input type="checkbox" 
+			    	className="toggle"
+			    	value = {this.state.completed}
+			    	onClick = {this.onTodoItemCheckedChange.bind(this)}
+			    />
+				<label>{this.props.todo.title}</label>
+			</li>
 		}
 	}
 
